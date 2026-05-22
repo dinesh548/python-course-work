7998ca69ec0ebfa84fa7f2c6834dd240ab9dc43b
Dinesh Tellamekala <tellamekaladinesh2004@gmail.com>
2026-05-22 11:37:33 +0530

    Add files via upload

---
+++ day1/day 1 pfs.py
@@ -0,0 +1,21 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> s="py"
+>>> s
+'py'
+>>> li = [10,20,30]
+>>> type(li)
+<class 'list'>
+>>> #tupple
+>>> t = (12.342,78,4587)
+>>> type(t)
+<class 'tuple'>
+>>> #set
+>>> s=(334,445,112,45)
+>>> s
+(334, 445, 112, 45)
+>>> d={'name':'porsche','vechile':'car','model':'2026'}
+>>> d
+{'name': 'porsche', 'vechile': 'car', 'model': '2026'}
+>>> type(d)
+<class 'dict'>
